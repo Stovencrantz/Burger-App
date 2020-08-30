5bdfd093d4dc2f780533fe597ee0fe24e325c744
Stovencrantz <sknapp143@gmail.com>
2020-08-30 17:43:59 -0400

    Added changes to accomodate for jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 //Establish mysql connection
 const mysql = require("mysql");
 require("dotenv").config();
-var connection = mysql.createConnection({
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
-    port: process.env.PORT || 3306,
     user: "root",
     password: process.env.MYSQL_KEY,
     database: "burgers_db"
   });
-  
+};
+
 
   // Make connection.
 connection.connect(function(err) {
